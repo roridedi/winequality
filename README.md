628663a023eef70da7ad56e64123e7bec59809ea
Ortech <ortech@roridedi-mbp.fios-router.home>
2020-02-09 13:02:22 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,38 @@
+## LAB 1
+
+###Prerequisites
+1. Python
+2. Pythorch
+3. sklearn
+
+Predicting A Good Quality Wine
+1. What are the components that go into factoring a good quality wine?
+>- Fixed Acidity 
+>- Volatile Acidity 
+>- Citric Acid 
+>- Residual Sugar 
+>- Chlorides 
+>- Free Sulfur Dioxide
+>- Total Sulfur Dioxide 
+>- Density
+>- pH
+>- Sulphates 
+>- Alcohol
+
+_The topic is subjective, more can be found here._
+>https://www.marketviewliquor.com/blog/2018/08/how-to-choose-a-good-wine/
+
+Problem Statement
+Find the correlation between the 11 wine properties and the outcome of quality as defined by the dataset.
+
+Purpose of DataScience
+The purpose of datascience is to make use of data and generate inferences in order to make better informed decisions.
+- The ability to take data — to be able to understand it, to process it, to extract value from it, to visualize it, to communicate it — that’s going to be a hugely important skill in the next decades.”
+Hal Varian
+
+Using Python & Tools
+
+
+
+Run APP
+`python winequality.py`
---
+++ winequality.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import collections
+from mpl_toolkits.mplot3d import Axes3D
+from itertools import combinations
+from sklearn.naive_bayes import GaussianNB
+from mpl_toolkits.mplot3d import axes3d
+
+# load dataset
+dataset = pd.read_csv("winequality-white.csv", delimiter=";")
+size = 4898#, 500, 1000]
+# This winery has decided to group wines in three different categories.
+# Low Quality
+dataset.loc[dataset['quality'] < 4, 'quality'] = 0
+# Mid Range
+dataset.loc[(dataset['quality'] == 5) | (dataset['quality'] == 6) | (dataset['quality'] == 7), 'quality'] = 1
+# High Quality
+dataset.loc[dataset['quality'] > 7, 'quality'] = 2
+
+X=dataset
+Y = dataset['quality']
+X= X.astype('int')
+Y=Y.astype('int')
+X= X.drop(['quality'],axis=1)
+
+# Attributes as described by the spreadsheet
+attributes = ["free sulfur dioxide","pH","sulphates","alcohol","residual sugar","density","citric acid",'chlorides',"total sulfur dioxide", "fixed acidity","volatile acidity" ]
+
+# Create a 3 dimensional diagram showing a correlation between the attributes and quality in order to make an inference
+fig = plt.figure()
+for i in range(len(attributes)):
+    arr = []
+    mean = np.mean(np.asarray(X.iloc[:,i]), axis=0)
+    sd = np.std(np.asarray(X.iloc[:,i]), axis=0)
+    for z in range(len(Y)):
+        newArr = [X.iloc[z,i] , Y[z] ]
+        arr.append(newArr)
+    c = collections.Counter(map(tuple, arr))
+
+
+    ax = fig.add_subplot(3,4,1+i, projection='3d')
+
+    for k, v in c.items():
+        ax.scatter(k[0], k[1], v, c='r', marker='o')
+
+    ax.set_xlabel(attributes[i])
+    ax.set_ylabel('Quality')
+    ax.set_zlabel('Frequency')
+
+plt.show()
